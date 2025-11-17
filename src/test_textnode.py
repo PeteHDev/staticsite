fa391faf00b6bd923c39e6284d8a04012ef35f57
Pete <petehal.dev@gmail.com>
2025-11-17 21:09:30 +0200

    Added split_into_code_nodes() function

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, split_into_code_nodes
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -28,6 +28,22 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertNotEqual(node, node2)
 
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_split_into_code_nodes(self):
+        node_no_code = TextNode("There is no code here", TextType.TEXT)
+        self.assertEqual(split_into_code_nodes(node_no_code), ["There is no code here"])
 
+        node_one_code_block = TextNode("Only one `code` block", TextType.TEXT)
+        self.assertEqual(split_into_code_nodes(node_one_code_block), ["Only one ", 
+                                                                      TextNode("code", TextType.CODE), 
+                                                                      " block"])
+        
+        node_all_code = TextNode("`ALL CODE`", TextType.TEXT)
+        self.assertEqual(split_into_code_nodes(node_all_code), [TextNode("ALL CODE", TextType.CODE)])
+
+        node_two_code_blocks = TextNode("`First code block` and `Second code block`", TextType.TEXT)
+        self.assertEqual(split_into_code_nodes(node_two_code_blocks), [TextNode("First code block", TextType.CODE),
+                                                                       " and ",
+                                                                       TextNode("Second code block", TextType.CODE)])
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 class TextType(Enum):
-    TEXT = "plain"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
@@ -20,3 +20,26 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
     
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for old in old_nodes:
+        if old.text_type != TextType.TEXT:
+            new_nodes.append(old)
+            continue
+
+        tmp_nodes = split_into_code_nodes(old)
+            
+
+
+def split_into_code_nodes(text_node, delimiter="`"):
+    split = text_node.text.split(delimiter)
+    L = len(split)
+    if L == 1:
+        return split
+    if L % 2 == 0:
+        raise Exception("invalid Markdown: code blocks should be enclosed in a pair of backticks <`>")
+              
+    for i in range(1, len(split), 2):
+        split[i] = TextNode(split[i], TextType.CODE)
+
+    return [node for node in split if (isinstance(node, str) and node != "") or not isinstance(node, str)]
