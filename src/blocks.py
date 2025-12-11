54582c1aead4dddac6ee9ac47bda40ddfcc826c2
Pete <petehal.dev@gmail.com>
2025-12-11 03:07:43 +0200

    Added block_to_block_type() function and unit tests for it.

--- src/blocks.py
+++ src/blocks.py
@@ -1,4 +1,13 @@
 import re
+from enum import Enum
+
+class BlockType(Enum):
+    P = 1
+    H = 2
+    CODE = 3
+    QUOTE = 4
+    U_LIST = 5
+    O_LIST = 6
 
 def markdown_to_blocks(markdown):
     newMarkdown = re.sub(r"\n[^\S\r\n]*", "\n", markdown)
@@ -6,3 +15,35 @@ def markdown_to_blocks(markdown):
     blocks = [item.strip() for item in blocks]
     blocks = [item for item in blocks if item]
     return blocks
+
+def block_to_block_type(block):
+    if re.match(r"^#{1,6} ", block):
+        return BlockType.H
+    elif block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+    
+    lines = block.split("\n")
+    quote = True
+    unordered_list = True
+    ordered_list = True
+    i = 0
+    while (quote or unordered_list or ordered_list) and i < len(lines):
+        if quote and not lines[i].startswith(">"):
+            quote = False
+        
+        if unordered_list and not lines[i].startswith("- "):
+            unordered_list = False
+
+        if ordered_list and not lines[i].startswith(f"{i+1}. "):
+            ordered_list = False
+        
+        i += 1
+
+    if quote:
+        return BlockType.QUOTE
+    elif unordered_list:
+        return BlockType.U_LIST
+    elif ordered_list:
+        return BlockType.O_LIST
+    else:
+        return BlockType.P
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from blocks import markdown_to_blocks
+from blocks import markdown_to_blocks, block_to_block_type, BlockType
 
 class TestBlocks(unittest.TestCase):
     def test_markdown_to_blocks_basic(self):
@@ -74,3 +74,91 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+class TestBlockType(unittest.TestCase):
+    def test_block_type_heading(self):
+        block1 = "# 1st level Heading"
+        block2 = "## 2nd level Heading"
+        block3 = "### 3rd level Heading"
+        block4 = "#### 4th level Heading"
+        block5 = "##### 5th level Heading"
+        block6 = "###### 6th level Heading"
+        block7 = "####### not a Heading"
+        block8 = "asd ### not a Heading"
+        block9 = "# H"
+        block10 = ""
+
+        self.assertEqual(block_to_block_type(block1), BlockType.H)
+        self.assertEqual(block_to_block_type(block2), BlockType.H)
+        self.assertEqual(block_to_block_type(block3), BlockType.H)
+        self.assertEqual(block_to_block_type(block4), BlockType.H)
+        self.assertEqual(block_to_block_type(block5), BlockType.H)
+        self.assertEqual(block_to_block_type(block6), BlockType.H)
+        self.assertEqual(block_to_block_type(block7), BlockType.P)
+        self.assertEqual(block_to_block_type(block8), BlockType.P)
+        self.assertEqual(block_to_block_type(block9), BlockType.H)
+        self.assertEqual(block_to_block_type(block10), BlockType.P)
+        
+    def test_block_type_code(self):
+        code = "```print('Hello, World!')\nprint('Hello, World! Again!')\nprint('Hello, World! Again...')```"
+        not_code = "``print('Hello, World!')``"
+
+        self.assertEqual(block_to_block_type(code), BlockType.CODE)
+        self.assertEqual(block_to_block_type(not_code), BlockType.P)
+
+    def test_block_type_quote(self):
+        quote = """>Some
+>Kind
+>of
+>Quote"""
+
+        not_quote = """>Some
+Kind
+>of
+Not Quote"""
+
+        self.assertEqual(block_to_block_type(quote), BlockType.QUOTE)
+        self.assertEqual(block_to_block_type(not_quote), BlockType.P)
+
+    def test_block_type_unordered_list(self):
+        unordered_list = """- Some
+- Kind
+- of
+- Unordered
+- List"""
+
+        not_unordered_list = """- Some
+Kind
+of
+- Unordered
+- List"""
+
+        self.assertEqual(block_to_block_type(unordered_list), BlockType.U_LIST)
+        self.assertEqual(block_to_block_type(not_unordered_list), BlockType.P)
+
+    def test_block_type_unordered_list(self):
+        ordered_list = """1. Some
+2. Kind
+3. of
+4. Unordered
+5. List"""
+        invalid_ordered_list1 = """2. Some
+3. Kind
+4. of
+5. Unordered
+6. List"""
+        invalid_ordered_list2 = """1. Some
+2. Kind
+4. of
+3. Unordered
+6. List"""
+        not_ordered_list = """1. Some
+Kind
+of
+2. Unordered
+3. List"""
+
+        self.assertEqual(block_to_block_type(ordered_list), BlockType.O_LIST)
+        self.assertEqual(block_to_block_type(invalid_ordered_list1), BlockType.P)
+        self.assertEqual(block_to_block_type(invalid_ordered_list2), BlockType.P)
+        self.assertEqual(block_to_block_type(not_ordered_list), BlockType.P)
